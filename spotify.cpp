c9e59ca73630349907a72ba7af7f3f2de60e9e0c
krilek <krilek@gmail.com>
2017-04-10 01:39:37 +0200

    recreated with functions, next up - proper removal of song duplicates

--- spotify.cpp
+++ spotify.cpp
@@ -1,123 +1,184 @@
-#include <stdio.h>
 #include <iostream>
 #include <curl/curl.h>
 #include <iomanip>
+#include <string>
 #include <vector>
 #include "json.hpp"
-#include "consoleapi.h"
 
 using namespace std;
 using json = nlohmann::json;
 
-size_t stringify(void *content, size_t size, size_t mem, void *userdata);
+std::vector<string> mainLibrary;
+std::vector<string> duplicates;
+CURL* rCurl;
+CURL* dCurl;
+bool error = false;
+struct Headers{
+	string auth = "Authorization: Bearer ";
+	string mime = "Accept: application/json";
+    string url = "https://api.spotify.com/v1/me/tracks?";
+};
 
-int main(int argc, char* argv[])
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
-	if(argc < 2) {
-		cout << "BRAK PARAMETROW" << endl;
-		cout << "Podaji auth token" << endl;
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+json createDelObj(unsigned int start, unsigned int amount, std::vector<string>* cont){
+	if(amount > 100 || amount <=0){
+		cerr << "Too big request for json list" << endl;
+		return NULL;
 	}else{
-		ConsoleApi songs(argv[1]);
-		songs.setOwner(argv[2]);
-		songs.setPlaylist(argv[3]);
-		json o;
-		struct curl_slist *params = NULL;
-		string response;
-		CURL *curl;
-		CURLcode res;
-		curl_global_init(CURL_GLOBAL_DEFAULT);
-		curl = curl_easy_init();
-		if(curl) {
-			params = curl_slist_append(params, songs.getMime());
-			params = curl_slist_append(params, songs.getAuth());
-			curl_easy_setopt(curl, CURLOPT_HTTPHEADER, params);
-			curl_easy_setopt(curl, CURLOPT_URL,songs.getUrl());
-			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, stringify);
-			curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
-			curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-			curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-			do {
-				/* Perform the request, res will get the return code */
-				res = curl_easy_perform(curl);
-				/* Check for errors */
-				if(res != CURLE_OK) {
-					fprintf(stderr, "curl_easy_perform() failed: %s\n",curl_easy_strerror(res));
-					break;
-				}
-				else{
-					o= json::parse(response);
-                    //cout << o;
-                    auto err = o.find("error");
-                    if(err == o.end()){
-						response.clear();
-						if(!o.at("items").empty()) {
-							for(unsigned int i=0; i<o.at("items").size(); i++) {
-								songs.insertSong(o.at("items").at(i).at("track").at("id"));
-							}
-							string next;
-							if(!o.at("next").is_null()) {
-								next = o.at("next");
-								curl_easy_setopt(curl, CURLOPT_URL,next.c_str());
-							}
-						}
+		json ret({});
+		for(unsigned int i = 0; i<amount;i++){
+			json buffer;
+			buffer["uri"] = "spotify:track:"+cont->at(start+i);
+			ret["track"].push_back(buffer);
+		}
+		return ret;
+	}
+}
+
+bool findDuplicate(string id){
+	std::vector<string>::iterator it;
+    it = find(mainLibrary.begin(), mainLibrary.end(), id);
+	if(it == mainLibrary.end()) return false;
+    else return true;
+}
+
+bool responseManip(string* response, std::vector<string>* cont, bool compare){
+	json obj;
+	if(response->length() > 2){
+		obj = json::parse(*response);
+		response->clear();
+		auto error = obj.find("error");
+		if(error != obj.end()){
+			std::cerr << "Error response" << '\n';
+			std::cerr << obj << '\n';
+			return false;
+		}else{
+			//Reading response
+			if(!obj.at("items").empty()) {
+				if(!compare){
+					for(unsigned int i=0; i<obj.at("items").size(); i++) {
+						cont->push_back(obj.at("items").at(i).at("track").at("id"));
+					}
+				}else{
+					for(unsigned int i=0; i<obj.at("items").size(); i++) {
+						if(findDuplicate(obj.at("items").at(i).at("track").at("id")))
+						cont->push_back(obj.at("items").at(i).at("track").at("id"));
 					}
 				}
-			} while(!o.at("next").is_null());
-
-			//Look for duplicates in playlist by comparison with librarySongs
-			curl_easy_setopt(curl, CURLOPT_URL,songs.getPlaylistUrl());
-			do {
-				/* Perform the request, res will get the return code */
-				res = curl_easy_perform(curl);
-				/* Check for errors */
-				if(res != CURLE_OK) {
-					fprintf(stderr, "curl_easy_perform() failed: %s\n",curl_easy_strerror(res));
-					break;
+				if(!obj.at("next").is_null()) {
+					string next = obj.at("next");
+					curl_easy_setopt(rCurl, CURLOPT_URL,next.c_str());
+					return true;
+				}else{
+					std::cerr << "next = null" << '\n';
+					return false;
 				}
-				else{
-					o= json::parse(response);
-					response.clear();
-                    auto err = o.find("error");
-                    if(err == o.end()){
-						if(!o.at("items").empty()) {
-							for(unsigned int i=0; i<o.at("items").size(); i++) {
-								//if(o.at("items").at(i).at("is_local") == true) continue;
+			}else{
+				std::cerr << "Empty object" << '\n';
+				return false;
+			}
+		}
+	}else{
+		std::cerr << "Too short response" << '\n';
+		return false;
+	}
+}
 
-								if(songs.findSong(o.at("items").at(i).at("track").at("id"))){
-									songs.playlistDuplicates.push_back(o.at("items").at(i).at("track").at("id"));
-									cout << "Duplikat" << endl;
-								}
-							}
-							string next;
-							if(!o.at("next").is_null()) {
-								next = o.at("next");
-								curl_easy_setopt(curl, CURLOPT_URL,next.c_str());
-							}
-						}
-					}
-				}
-			} while(!o.at("next").is_null());
-			/* always cleanup */
-			curl_easy_cleanup(curl);
+int main(int argc, char* argv[]){
+	Headers lHeaders;
+	Headers pHeaders;
+	if(argc > 3){
+		string auth = argv[1];
+		string login = argv[2];
+		string playlistID = argv[3];
+		lHeaders.auth += auth;
+		pHeaders.auth += auth;
+		pHeaders.url = "https://api.spotify.com/v1/users/" + login
+							+ "/playlists/" + playlistID + "/tracks";
+	}else{
+		std::cerr << "You need to pass parameters \n Required: \n oauth \n playlist owner login \n playlist ID" << '\n';
+		return 10;
+	}
+	struct curl_slist *params = NULL;
+	string readBuffer;
+	string* rBPointer = &readBuffer;
+	CURLcode res;
+	curl_global_init(CURL_GLOBAL_DEFAULT);
+	rCurl = curl_easy_init();
+	if(rCurl) {
+		params = curl_slist_append(params, lHeaders.mime.c_str());
+		params = curl_slist_append(params, lHeaders.auth.c_str());
+		curl_easy_setopt(rCurl, CURLOPT_HTTPHEADER, params);
+		curl_easy_setopt(rCurl, CURLOPT_URL,lHeaders.url.c_str());
+		curl_easy_setopt(rCurl, CURLOPT_WRITEFUNCTION, WriteCallback);
+		curl_easy_setopt(rCurl, CURLOPT_WRITEDATA, &readBuffer);
+		curl_easy_setopt(rCurl, CURLOPT_SSL_VERIFYPEER, 0L);
+		curl_easy_setopt(rCurl, CURLOPT_SSL_VERIFYHOST, 0L);
+		//Populate mainLibrary vector
+		std::vector<string>* libraryPtr;
+		libraryPtr = &mainLibrary;
+		do{
+			res = curl_easy_perform(rCurl);
+			if(res != CURLE_OK) {
+				std::cerr << "curl_easy_perform() failed: %s\n" << curl_easy_strerror(res);
+				break;
+			}
+		}while(responseManip(rBPointer, libraryPtr, false));
+		// for(auto lel : mainLibrary){
+		// 	cout << lel <<endl;
+		// }
+		curl_easy_setopt(rCurl, CURLOPT_URL,pHeaders.url.c_str());
+		libraryPtr = &duplicates;
+		do{
+			res = curl_easy_perform(rCurl);
+			if(res != CURLE_OK) {
+				std::cerr << "curl_easy_perform() failed: %s\n" << curl_easy_strerror(res);
+				break;
+			}
+		}while(responseManip(rBPointer, libraryPtr, true));
+		for(auto lel : duplicates){
+			cout << lel <<endl;
 		}
 
-		curl_global_cleanup();
+		//Remove duplicates
+		CURLcode delReturn;
+		dCurl = curl_easy_init();
+		curl_easy_setopt(dCurl, CURLOPT_CUSTOMREQUEST, "DELETE");
+		curl_easy_setopt(dCurl, CURLOPT_URL, pHeaders.url.c_str());
+		struct curl_slist *dParams = NULL;
+		dParams = curl_slist_append(params, lHeaders.mime.c_str());
+		dParams = curl_slist_append(params, lHeaders.auth.c_str());
+		curl_easy_setopt(dCurl, CURLOPT_HTTPHEADER, dParams);
+
+		//curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "name=daniel&project=curl");
+		unsigned int amount = libraryPtr->size();
+		unsigned int tens = amount%100;
+		cout << "TENS" << tens << "AMOUNT" << amount << endl;
+		string temp;
+		for(unsigned int i = 0; i<amount; i+=100){
+			if(i+100 > amount){
+				temp = createDelObj(i, tens,libraryPtr).dump();
+			}
+			else{
+				temp = createDelObj(i, i+100,libraryPtr).dump();
+			}
+			curl_easy_setopt(dCurl, CURLOPT_POSTFIELDS, temp.c_str());
+			delReturn = curl_easy_perform(rCurl);
+			if(delReturn != CURLE_OK) {
+				std::cerr << "curl_easy_perform() failed: %s\n" << curl_easy_strerror(res);
+				break;
+			}
+		}
+		curl_slist_free_all(params);
+		curl_slist_free_all(dParams);
+		curl_easy_cleanup(rCurl);
+		curl_easy_cleanup(dCurl);
 	}
+	curl_global_cleanup();
 	return 0;
 }
-
-size_t stringify(void *content, size_t size, size_t mem, void *userdata){
-	size_t wholesize = size* mem;
-	((string*)userdata)->append((char*)content, wholesize);
-	//cout << wielkosc;
-	return wholesize;
-}
-
-/*
-{ "tracks":[
-	{ "uri": "spotify:track:4iV5W9uYEdYUVa79Axb7Rh" },
-	{ "uri": "spotify:track:1301WleyT98MSxVHPZCA6M" }]
-}
-
-
-*/
